97b0e512b31e3f29863f5d79254a358c6e7583f1
INGANZAMARUMPU <jonkur96@gmail.com>
2021-06-02 22:25:07 +0200

    create first get urls

--- src/api/Index.java
+++ src/api/Index.java
@@ -7,7 +7,8 @@ import java.util.List;
 import models.*;
 
 import javax.ws.rs.GET;  
-import javax.ws.rs.Path;  
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -25,6 +26,34 @@ public class Index {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return Response.ok(villes).build();
+		return Response.ok(villes).header("Access-Control-Allow-Origin", "*").build();
+	}
+
+	@GET 
+	@Path("ville/{id}/")
+	@Produces({MediaType.APPLICATION_JSON})  
+	public Response getVille(@PathParam("id") String id) {
+		
+		Ville ville = null;
+		try {
+			ville = new DataBank().ville_dao.queryForId(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Response.ok(ville).header("Access-Control-Allow-Origin", "*").build();
+	}
+
+	@GET 
+	@Path("ville/{id}/appartements/")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response getVilleAppartement(@PathParam("id") String ville_id) {
+		
+		List<Appartement> appartements = new ArrayList();
+		try {
+			appartements = new DataBank().appartement_dao.queryForEq("ville_id", ville_id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Response.ok(appartements).header("Access-Control-Allow-Origin", "*").build();
 	}
 }   
